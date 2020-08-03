740465b9a997be5dfe34f9076ce7a0f778a82603
Robin Steeman <Robin_steeman@hotmail.com>
2020-08-03 09:29:17 +0200

    Updates

---
+++ precache-manifest.cfc1c5c15fbf20ca6c0e02e631082ffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e4967a8bab2380420cb107351677a00",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "14913d64fe59c028e048",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a2d4b0beb79c877939",
+    "url": "/game-of-life/static/js/main.750b8aa7.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
